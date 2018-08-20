cdd7256a0284680eef6b0106a9f553813c9dbf63
ProgMedia <anderson@progmedia.com.br>
2018-08-20 13:38:43 -0300

    Angular 4.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component'
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule
   ],
   providers: [],
