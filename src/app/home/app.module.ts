761dc8aa76b653008004cc1b882e3dcc1f13b848
ProgMedia <anderson@progmedia.com.br>
2018-08-29 12:40:40 -0300

    Add

--- src/app/home/app.module.ts
+++ src/app/home/app.module.ts
@@ -5,20 +5,23 @@ import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HeaderComponent } from '../header/header.component';
+import { HomeComponent } from './home.component';
+import { AboutComponent } from '../about/about.component';
+import { ROUTES} from './app.routes'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
